da1e8a666d37d128fac00928d3b9624fec0cefe1
Justin Tooley <jstooley823@gmail.com>
2017-09-06 06:33:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+require 'pry'
+def prime? (n)
+  array = []
+
+  (2...n).each_with_index do |num,i|
+    array[i] = num
+  end
+  
+  value = array.any? do |num|
+    (n % num) == 0
+    
+  end
+  not value
+end
