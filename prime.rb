e4fe3e88bb7934362f7daf5527ef90e7c6435410
Justin Tooley <jstooley823@gmail.com>
2017-09-06 06:37:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,7 @@
 # Add  code here!
 require 'pry'
 def prime? (n)
-  array = []
-
-  (2...n).each_with_index do |num,i|
-    array[i] = num
-  end
-
-  value = array.all?{|num|(n % num) == 0}
+  
+  value = (2...n).all?{|num|(n % num) == 0}
 binding.pry
 end
